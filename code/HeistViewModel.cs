54c6974e41fcb2a4599cac4bc87af4cb8be685a5
Yoran Mandema <yoranmandema@gmail.com>
2021-06-01 15:55:41 +0200

    Cleanup

--- code/HeistViewModel.cs
+++ code/HeistViewModel.cs
@@ -8,15 +8,15 @@ partial class HeistViewModel : BaseViewModel
 	Vector3 velocity;
 	Vector3 force; 
 	Vector3 acceleration;
-	float mouseScale = 1.5f;
-	float returnForce = 400f;
-	float damping = 20f;
-	float accelDamping = 0.2f;
-	float pivotForce = 1000f;
-	float rotationScale = 2.5f;
+	float MouseScale => 1.5f;
+	float ReturnForce => 400f;
+	float Damping => 20f;
+	float AccelDamping => 0.2f;
+	float PivotForce => 1000f;
+	float RotationScale => 2.5f;
 
-	Vector3 offset = new Vector3(2f,10,-3f);
-	float velocityClamp = 3f;
+	Vector3 Offset => new Vector3(2f,10,-3f);
+	float VelocityClamp => 3f;
 
 
 	public override void PostCameraSetup( ref CameraSetup camSetup )
@@ -43,38 +43,32 @@ partial class HeistViewModel : BaseViewModel
 			walkBob += Time.Delta * 30.0f * speed;
 		}
 
-		acceleration += Vector3.Left * -Local.Client.Input.MouseDelta.x * Time.Delta * mouseScale;
-		acceleration += Vector3.Up * -Local.Client.Input.MouseDelta.y * Time.Delta * mouseScale;
-		acceleration += -velocity * returnForce * Time.Delta;
+		acceleration += Vector3.Left * -Local.Client.Input.MouseDelta.x * Time.Delta * MouseScale;
+		acceleration += Vector3.Up * -Local.Client.Input.MouseDelta.y * Time.Delta * MouseScale;
+		acceleration += -velocity * ReturnForce * Time.Delta;
 
 		acceleration += Vector3.Forward * WalkCycle(0.5f, 5f) * speed * 0.2f;
 		acceleration += Vector3.Left * WalkCycle(0.5f, 1f)* speed * -0.25f;
 		acceleration += Vector3.Up * WalkCycle(0.5f, 10f) * speed * 0.25f;
 
-
 		velocity += acceleration * Time.Delta;
 
-		// velocity *= new Vector3(0,1,0);
+		ApplyDamping(ref acceleration, AccelDamping );
 
-		ApplyDamping(ref acceleration, accelDamping );
+		ApplyDamping( ref velocity, Damping );
 
-		ApplyDamping( ref velocity, damping );
-
-		velocity = velocity.Normal * Math.Clamp(velocity.Length, 0, velocityClamp);
-
-		DebugOverlay.ScreenText(new Vector2(100,100), 0, Color.Red, $"{Math.Round(acceleration.x,2)} {Math.Round(acceleration.y,2)} {Math.Round(acceleration.z,2)}");
-		DebugOverlay.ScreenText(new Vector2(100,100), 1, Color.Red, $"{velocity.x} {velocity.y} {velocity.z}");
+		velocity = velocity.Normal * Math.Clamp(velocity.Length, 0, VelocityClamp);
 
 		Rotation = Local.Pawn.EyeRot;
-		Rotation *= Rotation.FromYaw(velocity.y * rotationScale);
-		Rotation *= Rotation.FromRoll(-velocity.y * rotationScale);
-		Rotation *= Rotation.FromPitch(-velocity.z * rotationScale );
+		Rotation *= Rotation.FromYaw(velocity.y * RotationScale);
+		Rotation *= Rotation.FromRoll(-velocity.y * RotationScale);
+		Rotation *= Rotation.FromPitch(-velocity.z * RotationScale );
 
-		Position += forward * (velocity.x * 10f+ offset.x);
-		Position += left * (velocity.y* 10f + offset.y);
-		Position += up * (velocity.z* 10f + offset.z);
+		Position += forward * (velocity.x * 10f+ Offset.x);
+		Position += left * (velocity.y* 10f + Offset.y);
+		Position += up * (velocity.z* 10f + Offset.z);
 
-		Position += (Rotation.Forward - camSetup.Rotation.Forward) * -pivotForce;
+		Position += (Rotation.Forward - camSetup.Rotation.Forward) * -PivotForce;
 	}
 
 	private float WalkCycle (float speed, float power) {
@@ -83,15 +77,6 @@ partial class HeistViewModel : BaseViewModel
 
 	public void ApplyImpulse (Vector3 impulse) {
 		acceleration += impulse;
-	}
-
-	private void ApplyDamping( ref float value, float damping )
-	{
-		if ( value != 0 )
-		{
-			var drop = value * damping * Time.Delta;
-			value *= Math.Max( value - drop, 0 ) / value;
-		}
 	}
 
 	private void ApplyDamping( ref Vector3 value, float damping )
